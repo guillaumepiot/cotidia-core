4d7db46875c343fe43400836d94db6da7403aea3
Guillaume Piot <guillaume@cotidia.com>
2019-01-17 18:46:57 +0000

    Add migrations. Add filter to status list.

--- cotidia/core/mixins.py
+++ cotidia/core/mixins.py
@@ -3,12 +3,16 @@ from django.db import models
 from cotidia.core.models import Status
 
 
-class StatusModelMixin:
+class StatusModelMixin(models.Model):
     status = models.CharField(max_length=255, null=True)
+
+    class Meta:
+        abstract = True
 
     def status_set(self, status, notes=None, taxonomy=None, user=None):
         self.status = status
         self.save()
+        print("save status", self.status)
         kwargs = {
             "status": status,
             "notes": notes,
@@ -19,9 +23,12 @@ class StatusModelMixin:
         return Status.objects.create(**kwargs)
 
     def status_list(self, taxonomy=None):
-        return Status.objects.filter(
-            content_type=self.content_type, object_id=self.id, taxonomy=taxonomy
+        queryset = Status.objects.filter(
+            content_type=self.content_type, object_id=self.id
         )
+        if taxonomy:
+            queryset = queryset.filter(taxonomy=taxonomy)
+        return queryset
 
     def status_latest(self, taxonomy=None):
         return self.status_list(taxonomy=taxonomy).first()
